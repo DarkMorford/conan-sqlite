64ae5e063ddae14103a58b50a95ce55b10364925
DarkMorford <DarkMorford@users.noreply.github.com>
2017-07-21 17:40:25 -0700

    Build CLI and static library for Windows/MSVC.

--- conanfile.py
+++ conanfile.py
@@ -10,8 +10,8 @@ class SqliteConan(ConanFile):
     description = "SQLite is a self-contained, high-reliability, embedded, full-featured, public-domain, SQL database engine."
 
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "debug": [True, False]}
-    default_options = "shared=False", "debug=False"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
 
     def configure(self):
         del self.settings.compiler.libcxx
@@ -28,17 +28,37 @@ class SqliteConan(ConanFile):
         if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
             build_env = VisualStudioBuildEnvironment(self)
             with tools.environment_append(build_env.vars):
-                pass
+                vcvars = tools.vcvars_command(self.settings)
+
+                # Always build the command-line binary
+                self.run("%s && cl sqlite3.c shell.c -Fe:sqlite3.exe" % vcvars)
+
+                if self.options.shared:
+                    # Build shared library
+                    pass
+                else:
+                    # Build static library
+                    self.run("%s && cl /c sqlite3.c" % vcvars)
+                    self.run("%s && lib sqlite3.obj" % vcvars)
         else:
             raise Exception("Only MSVC compiler currently implemented.")
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        # Always copy headers
+        self.copy("*.h", dst="include", src="sqlite-amalgamation-3190300")
+
+        if self.settings.os == "Windows":
+            self.copy("sqlite3.exe", dst="bin", src="sqlite-amalgamation-3190300")
+            if self.options.shared:
+                pass
+            else:
+                self.copy("sqlite3.lib", dst="lib", src="sqlite-amalgamation-3190300")
+
+#        self.copy("*hello.lib", dst="lib", keep_path=False)
+#        self.copy("*.dll", dst="bin", keep_path=False)
+#        self.copy("*.so", dst="lib", keep_path=False)
+#        self.copy("*.dylib", dst="lib", keep_path=False)
+#        self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
         # Declare libraries that we generate
